87f57247147cb25c0ed50cd496e6582f180c5966
David Estes <davydotcom@gmail.com>
2014-12-16 19:47:01 -0500

    working on db connectivity

--- src/main/groovy/redchat/config/SecurityConfig.groovy
+++ src/main/groovy/redchat/config/SecurityConfig.groovy
@@ -1,10 +1,13 @@
-package redchat.config;
- 
+package redchat.config
+
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.*;
- 
+
+
 /**
  * An extremely basic auth setup for the sake of a demo project
  */
@@ -13,8 +16,14 @@ import org.springframework.security.config.annotation.web.configuration.*;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
  
   @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+  public void configureForDevelopment(AuthenticationManagerBuilder auth, Environment env) throws Exception {
     auth.inMemoryAuthentication().withUser("destes").password("password").roles("USER");
     auth.inMemoryAuthentication().withUser("user2").password("password").roles("USER");
   }
+
+//  @Autowired
+//  public void configureForProduction(AuthenticationManagerBuilder auth, DataSource dataSource, Environment env) throws Exception {
+//    // auth.inMemoryAuthentication().withUser("destes").password("password").roles("USER");
+//    // auth.inMemoryAuthentication().withUser("user2").password("password").roles("USER");
+//  }
 }
